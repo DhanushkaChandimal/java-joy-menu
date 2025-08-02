d24721027a83b1e5e24354d48569764b6e7a4441
Dhanushka <dhanushkamcr@gmail.com>
2025-08-02 17:57:24 -0500

    Rende components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Coffees from "./Coffees"
+import Pastries from "./Pastries"
+import Teas from "./Teas"
+import './MenuStyles.css';
+
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="menu-board">
+      <Coffees/>
+      <Teas/>
+      <Pastries/>
+    </div>
   )
 }
 
